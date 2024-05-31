8908280e9e914afc10e50626e508008af8db7e32
ColombusAH <hattabavner@gmail.com>
2024-05-31 10:27:02 +0300

    start fix

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -14,7 +14,7 @@ router.post("/login", api.withRemult, async (req, res) => {
     const { email, password } = req.body;
     const user = await usersCredentialsRepo.findOne({ where: { email } });
     if (!user) {
-        throw new Error('User not found');
+        throw new Error('Invalid credentials');
     }
     const isMatch = await bcrypt.compare(password, user.hashedPassword);
     if (!isMatch) {
@@ -22,6 +22,23 @@ router.post("/login", api.withRemult, async (req, res) => {
     }
     const token = jwt.sign({ id: user.id, tenant_id: user.tenantId }, secret, { expiresIn: '12h' });
     return token;
+});
+
+
+//register
+router.post("/register", api.withRemult, async (req, res) => {
+    const usersCredentialsRepo = remult.repo(UserCredentials);
+    const { email, password } = req.body;
+    const user = await usersCredentialsRepo.findOne({ where: { email } });
+    if (user) {
+        throw new Error('User already exists');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await usersCredentialsRepo.save({
+        email,
+        hashedPassword
+    });
+    return 'User created';
 });
 
 export default router;
